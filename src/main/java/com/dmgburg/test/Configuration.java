5e178af4b9dbd4c96f534ce42b16f29df35be90b
Trans00 <transformator00@mail.ru>
2017-07-23 23:45:03 +0300

    filtering works

--- src/main/java/com/dmgburg/test/Configuration.java
+++ src/main/java/com/dmgburg/test/Configuration.java
@@ -34,6 +34,15 @@ public class Configuration {
         return Collections.unmodifiableMap(rowMapping);
     }
 
+    /**
+     *
+     * Utility method to be used to parse mapping files and create proper Configuration object
+     *
+     * @param file - input tsv file with key -> value mapping in it
+     * @return Map key -> value extracted from file
+     * @throws IOException
+     */
+
     public static Map<String, String> parse(File file) throws IOException {
         List<String> configs = Files.readAllLines(file.toPath());
         return configs.stream()
@@ -45,17 +54,40 @@ public class Configuration {
         private Map<String, String> columnMapping;
         private Map<String, String> rowMapping;
 
+        /**
+         *
+         * @param columnMapping Map of external column name -> internal column name
+         * @return this
+         */
+
         public Builder setColumnMapping(Map<String, String> columnMapping) {
             this.columnMapping = columnMapping;
             return this;
         }
+
+        /**
+         *
+         * @param rowMapping - Map of external row id -> internal row id
+         * @return this
+         */
 
         public Builder setRowMapping(Map<String, String> rowMapping) {
             this.rowMapping = rowMapping;
             return this;
         }
 
+        /**
+         * Builds Configura
+         * @return Configaration object
+         */
+
         public Configuration build(){
+            if (columnMapping == null){
+                throw new IllegalStateException("Column mapping not set");
+            }
+            if (rowMapping == null){
+                throw new IllegalStateException("Row mapping not set");
+            }
             return new Configuration(columnMapping, rowMapping);
         }
     }
